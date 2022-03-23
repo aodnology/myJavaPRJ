ce30bb0b505f158a692fa1eae22e3077537e470c
Sooho park <aodnology@gmail.com>
2022-03-23 10:46:24 +0900

    class

---
+++ src/main/java/Ex_04.java
@@ -0,0 +1,15 @@
+public class Ex_04 {
+    public static void main(String[] args) {
+
+        String str1 = "IT CookBook 입니다";
+        String str2 = "10";
+        String str3 = "20";
+
+        str1 = "Java 입니다. ";
+
+        System.out.printf("%s \n", str1);
+        System.out.printf("%s \n", str2 + str3);
+
+    }
+}
+
